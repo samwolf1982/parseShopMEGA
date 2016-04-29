4051dda24a90bb2e56c0dd66800c01ae40b23d17
samwolf1982 <biryukwolf@yahoo.com>
2016-04-29 09:05:37 +0300

    relixe 1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # parseShopMEGA
 parse mega shop plus vk
+
+это парсим
+http://shophelp.ru/forum/showthread.php?t=94461&page=5
+http://anti-free.ru/forum/showthread.php?t=58815&page=131
+http://alitrust.ru/boasts?page=2     ok
+http://alitaomall.com/showthread.php?t=21990&page=393
+http://ru.itao.com/#
+https://vk.com/expressali
+https://vk.com/godnoten
+https://vk.com/doehalo
+https://vk.com/hitwow
+https://vk.com/kruuutuuu
+https://vk.com/things_aliexpress
+https://vk.com/find_best
--- js/main2.js
+++ js/main2.js
@@ -13,7 +13,13 @@
         event.preventDefault();
    
 
-          var d={'val1': 999}; // первий раз ненадо дни автоматом 2 
+
+console.log("BOBO");
+$d=
+var o = $xp(FIRST_RECORD_LINK, $d);
+
+console.log($o);
+/*          var d={'val1': 999}; // первий раз ненадо дни автоматом 2 
 
 $.ajax({
   url: 'php/scanx.php',
@@ -25,7 +31,7 @@ $.ajax({
     
   }
 });
-
+*/
 
       });
   });
@@ -68,3 +74,44 @@ console.log("Change");
 
 
 
+//--------
+function $xp(xPath, $scope) {
+    var selector = convertXPath(xPath);
+    return $(selector, $scope);
+}
+ 
+function convertXPath(x) {
+ 
+    //parse //*
+    x = replace(x, '//\\*', '');
+ 
+    //parse id
+    x = replace(x, '\\[@id="([^"]*)"\\]', '#$1');
+ 
+    //parse [1]
+    x = replace(x, '\\[1\\]', ':first');
+ 
+    //parse [n]
+    x = replace(x, '\\[([0-9]+)\\]', ':eq($1)');
+ 
+    //parse :eq's and lower 1
+    var z = x.split(':eq(');
+    x = z[0];
+    if (z.length > 1) {
+        for (var i = 1; i < z.length; i++) {
+            var end = z[i].indexOf(')');
+            var number = parseInt(z[i].substr(0, end)) - 1;
+            x = x + ':eq(' + number + z[i].substr(end);
+        }
+    }
+ 
+    //parse /
+    x = replace(x, '/', ' > ');
+ 
+    return x;
+}
+ 
+function replace(txt, r, w) {
+  var re = new RegExp(r, "g");
+  return txt.replace(re, w);
+}
