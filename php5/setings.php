7fccd84040fe923f7face147f8b96b777fe72c59
samwolf1982 <biryukwolf@yahoo.com>
2016-04-29 09:10:33 +0300

    clone folder must add proxi

---
+++ php5/setings.php
@@ -0,0 +1,15 @@
+<?php
+$GLOBALS['path_hash'] = '/opt/lampp/htdocs/testwp/php4/hasess.txt';
+$GLOBALS['path_to_WP'] = 'http://testfordel.atwebpages.com/api/create_post';
+
+$GLOBALS['path_to_WP'] = 'http://www.imageplugin.dev/api/create_post';
+$GLOBALS['curent_host'] = 'shophelp.ru';
+$GLOBALS['curent_host_full'] = 'http://shophelp.ru';
+$GLOBALS['author'] = 'root';
+$GLOBALS['full_update'] = false;
+$GLOBALS['counter'] = 0;  // просто счетчик
+$GLOBALS['forum']='http://shophelp.ru/forum/'
+
+
+
+?>
